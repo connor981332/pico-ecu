bac6251eed82c194478e79db91aea2568e2594e1
Connor Bunch <connorbunch@outlook.com>
2022-04-01 18:26:20 -0600

    continuing experimentation

--- ecu.c
+++ ecu.c
@@ -3,14 +3,32 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
-int main() {
-    //bi_decl(bi_program_description("This is a test binary."));
-    //bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+// THIS IS THE ENGINE CONFIGURATION
+#define NUM_CYLINDERS 4
+#define FIRING_ORDER {1, 2, 4, 3}
+#define DISPLACEMENT_CC 649
 
+double intake_air_velocity = 0;
+double intake_air_temperature = 0;
+double engine_temperature = 0;
+double engine_rpm = 0;
+double crank_position = 0;
+double pico_temperature = 0;
+
+void read_inputs() {
+
+}
+
+void trigger_events() {
+}
+
+int main() {
     stdio_init_all();
 
     while(1) {
-        sleep_ms(1000);
-        puts("This is the beginning of the ECU program");
+        read_inputs();
+        trigger_events();
+        printf("Cycle complete\n");
+        sleep_ms(10000);
     }
 }
